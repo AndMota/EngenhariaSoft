9456978288af82d1c2ff228d9e0d8068fa736400
Oromar Rezende <oromar@ice.ufjf.br>
2019-06-10 00:32:26 -0300

    Preço mudado de ',' para '.'

--- cadastroUsuario.php
+++ cadastroUsuario.php
@@ -343,7 +343,7 @@
                 }
                 else //((tam > 2) && (tam <= 5))
                 {
-                    campo.value = vr.substr(0, tam - 2) + ',' + vr.substr(tam - 2, tam);
+                    campo.value = vr.substr(0, tam - 2) + '.' + vr.substr(tam - 2, tam);
                 }
             }
         }
